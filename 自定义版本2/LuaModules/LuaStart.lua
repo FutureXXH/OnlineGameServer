07a1052b9e292b745e45b235113af4c55c3d68ae
XXH <2246611889@qq.com>
2022-05-12 14:20:24 +0800

    新增发送给指定模块消息参数

--- 自定义版本2/LuaModules/LuaStart.lua
+++ 自定义版本2/LuaModules/LuaStart.lua
@@ -3,8 +3,15 @@ print("[Lua]: lua文件打开成功!")
 ID = -1
 --使用这种来使重载时保留原值
 i = i or 0
-
-
+--ServerLuaLib.RegMessage(消息ID,模块ID)
+--关闭模块
+--ServerLuaLib.CloseModule(模块ID);
+--加载模块
+--ServerLuaLib.LoadNewModule(模块ID,模块文件名)
+--发送消息
+--ServerLuaLib.SendMessage(消息ID,数据,发送源模块ID,目标模块ID);目标模块ID填-1则发送给所有注册该消息的模块
+--获取时间戳
+--ServerLuaLib.GetTime();
 
 
 --在模块加载后调用 传入了模块ID号
@@ -12,15 +19,13 @@ function OnInit(id)
     ID = id
     print("[Lua]: luaManager初始化成功  ID: "..ID)
     --在lua中加载模块
-    ServerLuaLib.LoadNewModule(2000,"lua1.lua")
-    ServerLuaLib.LoadNewModule(3000,"lua1.lua") 
-    --关闭模块
-    ServerLuaLib.CloseModule(ID);
+    --ServerLuaLib.LoadNewModule(2000,"GameRoom.lua")
+
 end
 
-
+--一直调用
 function OnUpdate()
-    
+   
  
 end
 
